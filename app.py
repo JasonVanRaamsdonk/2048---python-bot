b07aa1bde5a14294e60255466c5df601c60f7eca
jason <c17764691@mydit.ie>
2019-10-10 19:58:29 +0100

    TODO - add all tile coordinates

---
+++ app.py
@@ -0,0 +1,18 @@
+from PIL import ImageGrab, ImageOps
+import pyautogui
+
+print(pyautogui.displayMousePosition())
+
+
+class Coordinates:  # @TODO add all tile coordinates
+    cord11 = (260, 420)
+    cord12 = (400, 420)
+    cord13 = (530, 420)
+    cord14 = (660, 420)
+    cord21 = (260, 550)
+    cord22 = (400, 550)
+    cord23 = (530, 550)
+    cord24 = (660, 550)
+    cord31 = (260, 700)
+    cord32 = (400, 700)
+    cord33 = ()
