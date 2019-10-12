67c5443ca0973bd7207fc4ee62c71d9b7eedb738
jason <c17764691@mydit.ie>
2019-10-12 22:40:03 +0100

    addition: coordinates for 24 inch monitor

--- app.py
+++ app.py
@@ -9,23 +9,46 @@ currentGrid = [0, 0, 0, 0,
                0, 0, 0, 0]
 
 
-class Coordinates:  # @TODO add all tile coordinates
-    cord11 = (300, 420)
+class Coordinates:
+    # cord11 = (300, 420)
+    # cord12 = (440, 420)
+    # cord13 = (575, 420)
+    # cord14 = (715, 420)
+
+    # cord21 = (300, 550)
+    # cord22 = (440, 550)
+    # cord23 = (575, 550)
+    # cord24 = (715, 550)
+
+    # cord31 = (300, 700)
+    # cord32 = (440, 700)
+    # cord33 = (575, 700)
+    # cord34 = (715, 700)
+
+    # cord41 = (300, 820)
+    # cord42 = (440, 820)
+    # cord43 = (575, 820)
+    # cord44 = (715, 820)  # laptop screen positions
+
+    cord11 = (300, 370)
     cord12 = (440, 420)
     cord13 = (575, 420)
     cord14 = (715, 420)
+
     cord21 = (300, 550)
     cord22 = (440, 550)
     cord23 = (575, 550)
     cord24 = (715, 550)
+
     cord31 = (300, 700)
     cord32 = (440, 700)
     cord33 = (575, 700)
     cord34 = (715, 700)
+
     cord41 = (300, 820)
     cord42 = (440, 820)
     cord43 = (575, 820)
-    cord44 = (715, 820)
+    cord44 = (715, 820)  # 24" monitor screen positions
 
     cordArray = [cord11, cord12, cord13, cord14,
                  cord21, cord22, cord23, cord24,
@@ -58,7 +81,10 @@ def get_grid():
     gray_image = ImageOps.grayscale(image)  # converts image to greyscale
 
     # enumerate to keep index to update grid
-    for index, cord in enumerate(Coordinates.cordArray):  # get all pixels at once (in grayscale)
+    for cord in Coordinates.cordArray:  # get all pixels at once (in grayscale)
         pixel = gray_image.getpixel(cord)  # get the value of a pixel at any coordinate
-        pos = Values.valueArray.index(pixel)
+        # pos = Values.valueArray.index(pixel)
+        print(f'tile {cord}, grayscale value: {pixel}')
 
+
+get_grid()
