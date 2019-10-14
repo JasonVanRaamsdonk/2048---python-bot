bc8e5da0b8df5b494765835fd895a8ac2a29af65
jason <c17764691@mydit.ie>
2019-10-14 21:16:07 +0100

    added pyautogui movement to screen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from PIL import ImageGrab, ImageOps
 import pyautogui
+import time
 
 from coordinates import Coordinates
 from values import Values
@@ -16,6 +17,11 @@ LEFT = 101
 DOWN = 102
 RIGHT = 103
 
+score_grid = [50, 30, 15, 5,
+              30, -10, 0, 0,
+              15, 0, 0, 0,
+              5, 0, 0, 0]
+
 
 def get_grid():
     image = ImageGrab.grab()  # get value of every colour on the screen
@@ -27,7 +33,7 @@ def get_grid():
         # print(f'pixel = {pixel}')
         pos = Values.valueArray.index(pixel)  # find index of value in array based on grayscale value
         # print(f'pos = {pos}')
-        print(f'tile {cord}, grayscale value: {pixel}')
+        # print(f'tile {cord}, grayscale value: {pixel}')
         if pos == 0:
             currentGrid[index] = 0
         else:
@@ -103,10 +109,109 @@ def get_next_grid(grid, move):
     return temp
 
 
-get_grid()
-display_grid(currentGrid)
-print('-'*20)
-display_grid(get_next_grid(currentGrid, RIGHT))
+def get_grid_score(grid):
+    score = 0
+    for i in range(4):
+        for j in range(4):
+            score += grid[4*i+j] * score_grid[4*i+j]
+    return score
+
+
+def get_best_move(grid):
+    """
+    get the next grid in all directions
+    and then compute the score
+    :param grid:
+    :return:
+    """
+    score_up = get_grid_score(get_next_grid(grid, UP))
+    score_down = get_grid_score(get_next_grid(grid, DOWN))
+    score_left = get_grid_score(get_next_grid(grid, LEFT))
+    score_right = get_grid_score(get_next_grid(grid, RIGHT))
+
+    if not move_validation(grid, UP):
+        score_up = 0
+    if not move_validation(grid, DOWN):
+        score_up = 0
+    if not move_validation(grid, LEFT):
+        score_up = 0
+    if not move_validation(grid, RIGHT):
+        score_up = 0
+
+    max_score = max(score_up, score_down, score_left, score_right)
+
+    if score_up == max_score:
+        return UP
+    elif score_down == max_score:
+        return DOWN
+    elif score_left == max_score:
+        return LEFT
+    else:
+        return RIGHT
+
+
+def perform_move(move):
+    """
+    functionality to allow the bot to move the grid
+    :param move:
+    :return:
+    """
+    if move == UP:
+        pyautogui.keyDown('up')
+        print('UP')  # @FIXME - grid get caught in UP move loop
+        time.sleep(0.5)
+        pyautogui.keyUp('up')
+    elif move == DOWN:
+        pyautogui.keyDown('down')
+        print('DOWN')
+        time.sleep(0.5)
+        pyautogui.keyUp('down')
+    elif move == LEFT:
+        pyautogui.keyDown('left')
+        print('LEFT')
+        time.sleep(0.5)
+        pyautogui.keyUp('left')
+    else:
+        pyautogui.keyDown('right')
+        print('RIGHT')
+        time.sleep(0.5)
+        pyautogui.keyUp('right')
+
+
+def main():
+    time.sleep(3)
+    while True:
+        get_grid()
+        perform_move(get_best_move(currentGrid))
+        time.sleep(0.1)
+
+
+def move_validation(grid, move):
+    """
+    check whether the return value of perform_move
+    is actually a valid move
+    :param grid:
+    :param move:
+    :return:
+    """
+    if get_next_grid(grid, move) == grid:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    main()
+
+
+# get_grid()
+# display_grid(currentGrid)
+# print('-'*20)
+# # display_grid(get_next_grid(currentGrid, RIGHT))
+# # print('-'*20)
+# print(get_grid_score(currentGrid))
+# perform_move(UP)
+
 
 # @TODO - add pyautogui move invokes to interact with browser game
 
--- values.py
+++ values.py
@@ -1,16 +1,29 @@
 class Values:
     empty = 195
-    two = 229
+    two = 230
     four = 225
-    eight = 190
-    sixteen = 172
-    thirtyTwo = 157
-    sixtyFour = 135
+    eight = 191
+    sixteen = 173
+    thirtyTwo = 159
+    sixtyFour = 138
     oneTwentyEight = 205
-    twoFiftySix = 201
-    fiveOneTwo = 197
+    twoFiftySix = 202
+    fiveOneTwo = 198
     oneZeroTwoFour = 193
     twoZeroFourEight = 189
+
+    # empty = 195
+    # two = 229
+    # four = 225
+    # eight = 190
+    # sixteen = 172
+    # thirtyTwo = 157
+    # sixtyFour = 135
+    # oneTwentyEight = 205
+    # twoFiftySix = 201
+    # fiveOneTwo = 197
+    # oneZeroTwoFour = 193
+    # twoZeroFourEight = 189
 
     valueArray = [empty, two, four, eight, sixteen,
                   thirtyTwo, sixtyFour, oneTwentyEight,
